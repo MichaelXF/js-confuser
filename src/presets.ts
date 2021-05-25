3277a8a9533c04a72ac3db355308bbcb7ef28e9e
Michael <minimike3682@gmail.com>
2021-05-25 17:18:16 -0400

    Create presets.ts

---
+++ src/presets.ts
@@ -0,0 +1,122 @@
+import { ObfuscateOptions } from "./index";
+
+/**
+ * - High Obfuscation preset.
+ * - **Average 90% performance reduction.**
+ *
+ * ## **`Disabled features`**
+ * - `eval` Use at your own risk!
+ *
+ * ### Potential Issues
+ * 1. *String Encoding* can corrupt files. Disabled `stringEncoding` manually if this happens.
+ * 2. *Dead Code* can bloat file size. Reduce or disable `deadCode`.
+ */
+const reduction_98_percent: ObfuscateOptions = {
+  target: "node",
+  preset: "high",
+
+  // heavy
+  dispatcher: true,
+  controlFlowFlattening: 0.75,
+
+  // extract
+  objectExtraction: true,
+
+  // variables
+  renameVariables: true,
+  identifierGenerator: "randomized",
+
+  // Simple
+  calculator: true,
+  deadCode: 0.25,
+
+  minify: true,
+  opaquePredicates: 0.75,
+
+  duplicateLiteralsRemoval: 0.75,
+  globalConcealing: true,
+  stringConcealing: true,
+  stringEncoding: true,
+  stringSplitting: 0.75,
+
+  // Use at own risk!
+  eval: false,
+  flatten: true,
+};
+
+/**
+ * - Medium Obfuscation preset.
+ * - Average 50% performance reduction.
+ */
+const reduction_52_percent: ObfuscateOptions = {
+  target: "node",
+  preset: "medium",
+
+  // heavy
+  dispatcher: 0.75,
+  controlFlowFlattening: 0.5,
+
+  // extract
+  objectExtraction: true,
+
+  // variables
+  renameVariables: true,
+  identifierGenerator: "randomized",
+
+  // Simple
+  calculator: true,
+  deadCode: 0.05,
+
+  minify: true,
+  opaquePredicates: 0.5,
+
+  duplicateLiteralsRemoval: 0.5,
+  globalConcealing: true,
+  stringConcealing: true,
+
+  // Use at own risk!
+  eval: false,
+};
+
+/**
+ * - Low Obfuscation preset.
+ * - Average 30% performance reduction.
+ */
+const reduction_30_percent: ObfuscateOptions = {
+  target: "node",
+  preset: "low",
+
+  // heavy
+  dispatcher: 0.5,
+  controlFlowFlattening: 0.25,
+
+  // extract
+  objectExtraction: true,
+
+  // variables
+  renameVariables: true,
+  identifierGenerator: "randomized",
+
+  // Simple
+  calculator: true,
+
+  minify: true,
+  opaquePredicates: true,
+
+  duplicateLiteralsRemoval: true,
+  globalConcealing: true,
+
+  // Use at own risk!
+  eval: false,
+};
+
+/**
+ * Built-in obfuscator presets.
+ */
+const presets = {
+  high: reduction_98_percent,
+  medium: reduction_52_percent,
+  low: reduction_30_percent,
+};
+
+export default presets;
