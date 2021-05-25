f568d77f049ec9bb3b5e195da0bf82715906755b
Michael <minimike3682@gmail.com>
2021-05-25 17:12:51 -0400

    Create high.js

---
+++ samples/high.js
@@ -0,0 +1,77 @@
+var LrJ_bNQ = [
+    2,
+    5,
+    33,
+    0,
+    '\x3c\x7e\x43\x69\x3c\x71\x7e\x3e',
+    1,
+    4,
+    3,
+    616,
+    11,
+    330
+  ], l_Mt$X = '\x3c\x7e\x3c\x2b\x6f\x68\x63\x46\x29\x51\x32\x41\x40\x71\x3f\x63\x6d\x44\x65\x2a\x45\x25\x42', nrn3j$A = '\x3c\x7e\x30\x64\x25\x74\x68\x30\x64';
+l_Mt$X += '\x6c\x62\x44\x38\x44\x4a\x58\x53', l_Mt$X += '\u0040\u0046\u0044\u002c\u005d\u002b\u0041\u004b\u0059\u0066\u0027\u0044\u0049\u006d\u006c\u0033\u0043\u0069', l_Mt$X += '\u003d\u0033\u0028\u002b\u0054', l_Mt$X += '\u007e\u003e', nrn3j$A += '\u0028\u0031\u004f\u002b\u0039\u007e\u003e';
+var JoTrLd = l_Mt$X => {
+    var nrn3j$A, JoTrLd, Q5mZ44D, wMTgJVE, zoHgsV, IbFiBCb = String, flATlMV = '\x6c\x65\x6e\x67\x74\x68', qmxk$Wx = 255, Kw61Km1 = '\x63\x68\x61\x72\x43\x6f\x64\x65\x41\x74', WPBgMup = '\x73\x6c\x69\x63\x65', TZweGyO = '\u0072\u0065\u0070\u006c\u0061\u0063\u0065';
+    for ('\u003c\u007e' === l_Mt$X[WPBgMup](0, LrJ_bNQ[0]) && '\x7e\x3e' === l_Mt$X[WPBgMup](-LrJ_bNQ[0]), l_Mt$X = l_Mt$X[WPBgMup](LrJ_bNQ[0], -LrJ_bNQ[0])[TZweGyO](/s/g, '')[TZweGyO]('\x7a', '\x21\x21\x21\x21\x21'), nrn3j$A = '\x75\x75\x75\x75\x75'[WPBgMup](l_Mt$X[flATlMV] % 5 || LrJ_bNQ[1]), l_Mt$X += nrn3j$A, Q5mZ44D = [], wMTgJVE = LrJ_bNQ[3], zoHgsV = l_Mt$X[flATlMV]; zoHgsV > wMTgJVE; wMTgJVE += LrJ_bNQ[1])
+      JoTrLd = 52200625 * (l_Mt$X[Kw61Km1](wMTgJVE) - LrJ_bNQ[2]) + 614125 * (l_Mt$X[Kw61Km1](wMTgJVE + LrJ_bNQ[5]) - 33) + 7225 * (l_Mt$X[Kw61Km1](wMTgJVE + LrJ_bNQ[0]) - 33) + 85 * (l_Mt$X[Kw61Km1](wMTgJVE + LrJ_bNQ[7]) - LrJ_bNQ[2]) + (l_Mt$X[Kw61Km1](wMTgJVE + LrJ_bNQ[6]) - LrJ_bNQ[2]), Q5mZ44D.push(qmxk$Wx & JoTrLd >> 24, qmxk$Wx & JoTrLd >> 16, qmxk$Wx & JoTrLd >> 8, qmxk$Wx & JoTrLd);
+    return ((l_Mt$X, Q5mZ44D) => {
+      for (var nrn3j$A = Q5mZ44D; nrn3j$A > LrJ_bNQ[3]; nrn3j$A--)
+        l_Mt$X.pop();
+    })(Q5mZ44D, nrn3j$A[flATlMV]), IbFiBCb.fromCharCode.apply(IbFiBCb, Q5mZ44D);
+  }, Q5mZ44D = [
+    '\x3c\x7e\x40\x72\x48\x37\x2b\x44\x65\x72\x74\x7e\x3e',
+    LrJ_bNQ[4],
+    l_Mt$X,
+    LrJ_bNQ[4],
+    nrn3j$A
+  ], wMTgJVE = LrJ_bNQ =>
+    (JoTrLd(Q5mZ44D[LrJ_bNQ]));
+function zoHgsV() {
+  try {
+    return global;
+  } catch (LrJ_bNQ) {
+    return this;
+  }
+}
+var IbFiBCb = zoHgsV.call(this), flATlMV = l_Mt$X => {
+    switch (l_Mt$X) {
+    case LrJ_bNQ[8]:
+      return IbFiBCb[wMTgJVE(LrJ_bNQ[3])];
+    }
+  }, qmxk$Wx = (LrJ_bNQ, l_Mt$X, nrn3j$A) => {
+    var JoTrLd = LrJ_bNQ * 207 + -89;
+    switch (JoTrLd) {
+    case 68221:
+      return l_Mt$X + nrn3j$A;
+    }
+  }, Kw61Km1 = 7;
+while (Kw61Km1 != LrJ_bNQ[9]) {
+  var WPBgMup = Kw61Km1 * 191 + -143;
+  switch (WPBgMup) {
+  case 1194:
+    Kw61Km1 += 9;
+    break;
+  case 2913:
+    var TZweGyO = qmxk$Wx(LrJ_bNQ[10], LrJ_bNQ[5], LrJ_bNQ[5]);
+    Kw61Km1 += LrJ_bNQ[6];
+    break;
+  case 3677:
+    Kw61Km1 -= LrJ_bNQ[7];
+    break;
+  case 430:
+    Kw61Km1 += 8;
+    break;
+  case 1958:
+    Kw61Km1 += 6;
+    break;
+  case 4059:
+    flATlMV(LrJ_bNQ[8])[wMTgJVE(LrJ_bNQ[5])](wMTgJVE(LrJ_bNQ[0]));
+    Kw61Km1 -= LrJ_bNQ[9];
+    break;
+  case 3104:
+    flATlMV(LrJ_bNQ[8])[wMTgJVE(3)](qmxk$Wx(LrJ_bNQ[10], wMTgJVE(4), TZweGyO));
+    Kw61Km1 += LrJ_bNQ[1];
+  }
+}
