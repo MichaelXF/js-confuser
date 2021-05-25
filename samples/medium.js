c8ab0fcf376fa6b82e2f1e56bd08879ba40bf5fc
Michael <minimike3682@gmail.com>
2021-05-25 17:13:15 -0400

    Create medium.js

---
+++ samples/medium.js
@@ -0,0 +1,40 @@
+var nB8lAM = [
+    2,
+    201,
+    913,
+    3
+  ], PYscs0 = PYscs0 => {
+    var JUIAN0W, WyyGlQw, QIdxFP, EVbSsWq, WrKBOQ, GYIGBd = String, MdX863 = 'length', hqBYHj = 255, BscAVz = 'charCodeAt', YcnR9gC = 'slice', QsnZMo = 'replace';
+    for ('<~' === PYscs0[YcnR9gC](0, 2) && '~>' === PYscs0[YcnR9gC](-nB8lAM[0]), PYscs0 = PYscs0[YcnR9gC](nB8lAM[0], -2)[QsnZMo](/s/g, '')[QsnZMo]('z', '!!!!!'), JUIAN0W = 'uuuuu'[YcnR9gC](PYscs0[MdX863] % 5 || 5), PYscs0 += JUIAN0W, QIdxFP = [], EVbSsWq = 0, WrKBOQ = PYscs0[MdX863]; WrKBOQ > EVbSsWq; EVbSsWq += 5)
+      WyyGlQw = 52200625 * (PYscs0[BscAVz](EVbSsWq) - 33) + 614125 * (PYscs0[BscAVz](EVbSsWq + 1) - 33) + 7225 * (PYscs0[BscAVz](EVbSsWq + 2) - 33) + 85 * (PYscs0[BscAVz](EVbSsWq + nB8lAM[3]) - 33) + (PYscs0[BscAVz](EVbSsWq + 4) - 33), QIdxFP.push(hqBYHj & WyyGlQw >> 24, hqBYHj & WyyGlQw >> 16, hqBYHj & WyyGlQw >> 8, hqBYHj & WyyGlQw);
+    return ((PYscs0, QIdxFP) => {
+      for (var JUIAN0W = QIdxFP; JUIAN0W > 0; JUIAN0W--)
+        PYscs0.pop();
+    })(QIdxFP, JUIAN0W[MdX863]), GYIGBd.fromCharCode.apply(GYIGBd, QIdxFP);
+  }, JUIAN0W = [
+    '<~@rH7+Dert~>',
+    '<~Ci<q~>',
+    '<~0d%th0d(1O+9~>',
+    '<~Ci<q~>',
+    '<~<+ohcF)Q2A@q?cmDe*E%BlbD8DJXS@FD,]+AKYf\'DIml3Ci=3(+T~>'
+  ], WyyGlQw = nB8lAM =>
+    (PYscs0(JUIAN0W[nB8lAM]));
+function QIdxFP() {
+  try {
+    return global;
+  } catch (nB8lAM) {
+    return this;
+  }
+}
+var EVbSsWq = QIdxFP.call(this), WrKBOQ = PYscs0 => {
+    switch (PYscs0) {
+    case -nB8lAM[1]:
+      return EVbSsWq[WyyGlQw(0)];
+    }
+  }, GYIGBd = (PYscs0, JUIAN0W, WyyGlQw) => {
+    switch (PYscs0) {
+    case -nB8lAM[2]:
+      return JUIAN0W + WyyGlQw;
+    }
+  }, MdX863 = GYIGBd(-913, 1, 1);
+WrKBOQ(-nB8lAM[1])[WyyGlQw(1)](GYIGBd(-nB8lAM[2], WyyGlQw(2), MdX863)), WrKBOQ(-nB8lAM[1])[WyyGlQw(nB8lAM[3])](WyyGlQw(4));
