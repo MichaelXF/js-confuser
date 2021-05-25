749142f9b8dec124225aab25935f59a98189d259
Michael <minimike3682@gmail.com>
2021-05-25 17:13:50 -0400

    Create low.js

---
+++ samples/low.js
@@ -0,0 +1,26 @@
+var UvB2KGE = [
+  1,
+  872,
+  981,
+  'log'
+];
+function Id15DvL() {
+  try {
+    return global;
+  } catch (UvB2KGE) {
+    return this;
+  }
+}
+var aXM92rd = Id15DvL.call(this), Tuaxwt = Id15DvL => {
+    switch (Id15DvL) {
+    case -UvB2KGE[1]:
+      return aXM92rd.console;
+    }
+  }, h6wvCVT = (UvB2KGE, Id15DvL, aXM92rd) => {
+    var Tuaxwt = UvB2KGE * -150 + -89;
+    switch (Tuaxwt) {
+    case -147239:
+      return Id15DvL + aXM92rd;
+    }
+  }, S7MWv1 = h6wvCVT(UvB2KGE[2], UvB2KGE[0], UvB2KGE[0]);
+Tuaxwt(-UvB2KGE[1])[UvB2KGE[3]](h6wvCVT(UvB2KGE[2], '1 + 1 is ', S7MWv1)), Tuaxwt(-UvB2KGE[1])[UvB2KGE[3]]('The source code is only three lines long!');
