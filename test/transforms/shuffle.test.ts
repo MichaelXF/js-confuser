84eb8cb8096d2e94ef387ba0479aac668219aeb1
MichaelXF <minimike3682@gmail.com>
2021-05-28 08:51:22 -0400

    fixed shuffle test

--- test/transforms/shuffle.test.ts
+++ test/transforms/shuffle.test.ts
@@ -9,7 +9,26 @@ it("should result in the same order", async () => {
 
   var output = await JsConfuser(code, {
     target: "browser",
-    stringConcealing: true,
+    shuffle: true,
+  });
+
+  var value;
+  function input(valueIn) {
+    value = valueIn;
+  }
+
+  eval(output);
+
+  expect(value).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
+it("should properly shuffle arrays within expressions", async () => {
+  var code = `
+    input([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  `;
+
+  var output = await JsConfuser(code, {
+    target: "browser",
+    shuffle: true,
   });
 
   var value;
