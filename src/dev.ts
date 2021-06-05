ab0dce3e3f6d10e12375cd40fc88d04f01eb4a2d
MichaelXF <minimike3682@gmail.com>
2021-06-05 11:39:42 -0400

    changed order of opaque predicates

--- src/dev.ts
+++ src/dev.ts
@@ -3,27 +3,30 @@ import JsConfuser from "./index";
 
 JsConfuser.obfuscate(
   `
-  function fibonacci(num){   
-    var a = 0, b = 1, c = num;
-    while (num-- > 1) {
-      c = a + b;
-      a = b;
-      b = c;
-    }
-    return c;
-  }
+  console.log(1)
+  console.log(2)
+  console.log(3)
+  console.log(4)
+  console.log(5)
+  console.log(6)
 
-  for ( var i = 1; i <= 25; i++ ) {
-    console.log(i, fibonacci(i))
-  }
 `,
   {
     target: "node",
-    preset: "high",
-    stringEncoding: false,
+    opaquePredicates: true,
+    controlFlowFlattening: true,
+    renameVariables: true,
+    identifierGenerator: {
+      hexadecimal: 0.25,
+      randomized: 0.25,
+      number: 0.25,
+      mangled: 0.25,
+    },
+    minify: true,
+    verbose: true,
   }
 ).then((obfuscated) => {
   console.log(obfuscated);
   eval(obfuscated);
-  writeFileSync("./dev.error.js", obfuscated, { encoding: "utf-8" });
+  // writeFileSync("./dev.error.js", obfuscated, { encoding: "utf-8" });
 });
