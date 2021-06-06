ffc3a64c203b1b9afaee61f2a28afaf9282fc51a
MichaelXF <minimike3682@gmail.com>
2021-06-06 18:21:00 -0400

    use chained calls with nested index shifting like javascript-obfuscator's `stringArrayWrappersChainedCalls`

--- src/dev.ts
+++ src/dev.ts
@@ -3,15 +3,40 @@ import JsConfuser from "./index";
 
 var code = `
 console.log(1)
-console.log(2)
-console.log(3)
-console.log(4)
-console.log(5)
-console.log(6)
-console.log(7)
-console.log(8)
-console.log(9)
-console.log(10)
+console.log(1)
+if ( true ) {
+  console.log(1)
+  console.log(1)
+  console.log(1)
+}
+
+if( true ) {
+  console.log(1)
+  console.log(1)
+  console.log(1)
+  console.log(1)
+  if( true ) {
+    console.log(1)
+    console.log(1)
+    console.log(1)
+    console.log(1)
+    if( true ) {
+      console.log(1)
+      console.log(1)
+      console.log(1)
+      console.log(1)
+      if( true ) {
+        console.log(1)
+        console.log(1)
+        console.log(1)
+        console.log(1)
+      }
+    }
+    
+  }
+}
+
+
 
 
 
@@ -21,10 +46,8 @@ eval(code);
 
 JsConfuser.obfuscate(code, {
   target: "node",
-  controlFlowFlattening: true,
+  duplicateLiteralsRemoval: true,
   compact: false,
-  renameVariables: true,
-  identifierGenerator: "mangled",
 }).then((output) => {
   console.log(output);
   writeFileSync("./dev.error.js", output, { encoding: "utf-8" });
