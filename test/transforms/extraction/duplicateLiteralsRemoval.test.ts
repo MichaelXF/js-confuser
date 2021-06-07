2545ce164598df9392e4c3a11785ab2221f338f5
MichaelXF <minimike3682@gmail.com>
2021-06-07 17:29:19 -0400

    added test

--- test/transforms/extraction/duplicateLiteralsRemoval.test.ts
+++ test/transforms/extraction/duplicateLiteralsRemoval.test.ts
@@ -14,3 +14,24 @@ it("should remove duplicate literals", async () => {
   expect(output).not.toContain("5,5");
   expect(output).toContain("5");
 });
+
+it("should remove duplicate literals and execute correctly", async () => {
+  var code = `
+  
+  TEST_ARRAY = [5,5];
+  `;
+
+  var output = await JsConfuser(code, {
+    target: "browser",
+    duplicateLiteralsRemoval: true,
+  });
+
+  expect(output).not.toContain("5,5");
+  expect(output).toContain("5");
+
+  var TEST_ARRAY;
+
+  eval(output);
+
+  expect(TEST_ARRAY).toEqual([5, 5]);
+});
