1d4cd9969f98aaac335e7b413cbaf03a9ce2f608
MichaelXF <minimike3682@gmail.com>
2021-06-17 02:00:27 -0400

    Improved crashing methods

--- src/templates/crash.ts
+++ src/templates/crash.ts
@@ -1,9 +1,14 @@
 import Template from "./template";
 
-
-
 export const CrashTemplate1 = Template(`
-Math.random() > 0.5 && process && process.exit();
+try {
+    if(typeof process !== "undefined") {
+        Math.random() > 0.5 && process && process.exit();
+    }
+} catch ( e ) {
+    
+}
+
 var {var} = "a";
 while(1){
     {var} = {var} += "a";    //add as much as the browser can handle
@@ -21,20 +26,34 @@ while(true) {
  };`);
 
 export const CrashTemplate3 = Template(`
-function {$2}(y, x){
-  return x;
-}
-
-var {$1} = {$2}(this, function () {
-  var {$3} = function () {
-      var regExp = {$3}
-          .constructor('return /" + this + "/')()
-          .constructor('^([^ ]+( +[^ ]+)+)+[^ ]}');
+try {
+    function {$2}(y, x){
+        return x;
+      }
       
-      return !regExp.call({$1});
-  };
-  
-  return {$3}();
-});
+      var {$1} = {$2}(this, function () {
+        var {$3} = function () {
+            var regExp = {$3}
+                .constructor('return /" + this + "/')()
+                .constructor('^([^ ]+( +[^ ]+)+)+[^ ]}');
+            
+            return !regExp.call({$1});
+        };
+        
+        return {$3}();
+      });
+      
+      {$1}();
+} catch ( e ) {
+    while(e ? e : !e){
+        var b;
+        var c = 0;
+        (e ? !e : e) ? (function(e){
+            c = e ? 0 : !e ? 1 : 0;
+        })(e) : b = 1;
 
-{$1}();`);
+        if(b&&c){break;}
+        if(b){continue;}
+    }
+}
+`);
