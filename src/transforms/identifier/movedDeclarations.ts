f6e695c9660d4f5a9dd1b4b9118fecdaac3895f3
MichaelXF <minimike3682@gmail.com>
2021-06-21 10:08:41 -0400

    Improved performance

--- src/transforms/identifier/movedDeclarations.ts
+++ src/transforms/identifier/movedDeclarations.ts
@@ -15,6 +15,7 @@ import { ok } from "assert";
 import { ObfuscateOrder } from "../../order";
 import { getIdentifierInfo } from "../../util/identifiers";
 import { isLoop } from "../../util/compare";
+import { reservedIdentifiers } from "../../constants";
 
 /**
  * Defines all the names at the top of every lexical block.
@@ -39,98 +40,95 @@ export default class MovedDeclarations extends Transform {
       var definingIdentifiers = new Map<string, Node>();
 
       walk(object, parents, (o: Node, p: Node[]) => {
-        if (o.type == "Identifier" && !illegal.has(o.name)) {
+        if (
+          o.type == "Identifier" &&
+          !reservedIdentifiers.has(o.name) &&
+          !this.options.globalVariables.has(o.name) &&
+          !illegal.has(o.name)
+        ) {
           var info = getIdentifierInfo(o, p);
-          if (info.spec.isDefined && definingIdentifiers.has(o.name)) {
-            illegal.add(o.name);
-            this.log(o.name, "is illegal due to detected being redefined");
-          }
-          if (info.spec.isDefined && p.find((x) => x.type == "SwitchCase")) {
-            illegal.add(o.name);
-            this.log(o.name, "is illegal due being in switch case");
+          if (!info.spec.isReferenced) {
+            return;
           }
 
           if (o.hidden) {
             illegal.add(o.name);
+          } else if (info.spec.isDefined) {
+            if (
+              definingIdentifiers.has(o.name) ||
+              p.find((x) => x.type == "SwitchCase")
+            ) {
+              illegal.add(o.name);
+            }
           }
         }
 
-        var s = getBlock(o, p);
-        if (s == block) {
+        if (
+          o.type == "VariableDeclaration" &&
+          o.declarations.length &&
+          o.kind === "var" &&
+          !o.declarations.find(
+            (x) => x.id.type !== "Identifier" || illegal.has(x.id.name)
+          )
+        ) {
+          var s = getBlock(o, p);
+          if (s != block) {
+            return;
+          }
           return () => {
-            if (
-              o.type == "VariableDeclaration" &&
-              o.declarations.length &&
-              o.kind !== "let" &&
-              !o.declarations.find(
-                (x) => x.id.type !== "Identifier" || illegal.has(x.id.name)
-              )
-            ) {
-              var index = block.body.indexOf(o);
-              if (index === 0 || o.hidden) {
-                o.declarations.forEach((x) => {
-                  illegal.add(x.id.name);
-                });
-                this.log(
-                  o.declarations.map((x) => x.id.name).join(", "),
-                  "is/are illegal due to already being at the top"
-                );
-                return;
-              }
-
-              if (isForInitialize(o, p)) {
-                this.log(
-                  o.declarations.map((x) => x.id.name).join(", "),
-                  "is/are illegal due to being in for initializer"
-                );
-                return;
-              }
-
-              var isIllegal = false;
+            var index = block.body.indexOf(o);
+            if (index === 0 || o.hidden) {
               o.declarations.forEach((x) => {
-                if (varNames.has(x.id.name)) {
-                  illegal.add(x.id.name);
-                  isIllegal = true;
+                illegal.add(x.id.name);
+              });
+              return;
+            }
 
-                  this.log(
-                    x.id.name,
-                    "is illegal due to already being defined"
-                  );
-                }
+            if (isForInitialize(o, p)) {
+              return;
+            }
+
+            var isIllegal = false;
+            o.declarations.forEach((x) => {
+              if (varNames.has(x.id.name)) {
+                illegal.add(x.id.name);
+                isIllegal = true;
+
+                this.log(x.id.name, "is illegal due to already being defined");
+              }
+            });
+
+            if (!isIllegal) {
+              varDecs.push([o, p]);
+
+              var names = [];
+
+              o.declarations.forEach((x) => {
+                ok(x.id.name);
+                varNames.add(x.id.name);
+
+                names.push(x.id.name);
+
+                definingIdentifiers.set(x.id.name, x.id);
               });
 
-              if (!isIllegal) {
-                varDecs.push([o, p]);
+              // Change this line to assignment expressions
 
-                o.declarations.forEach((x) => {
-                  ok(x.id.name);
-                  varNames.add(x.id.name);
+              var assignmentExpressions = o.declarations.map((x) =>
+                AssignmentExpression(
+                  "=",
+                  Identifier(x.id.name),
+                  clone(x.init) || Identifier("undefined")
+                )
+              );
 
-                  definingIdentifiers.set(x.id.name, x.id);
-                });
+              ok(assignmentExpressions.length, "Should be at least 1");
 
-                // Change this line to assignment expressions
+              var value: Node = SequenceExpression(assignmentExpressions);
 
-                var assignmentExpressions = o.declarations.map((x) =>
-                  AssignmentExpression(
-                    "=",
-                    clone(x.id),
-                    clone(x.init) || Identifier("undefined")
-                  )
-                );
+              value = ExpressionStatement(value);
 
-                ok(assignmentExpressions.length, "Should be at least 1");
-
-                var value: Node = SequenceExpression(assignmentExpressions);
-
-                value = ExpressionStatement(value);
-
-                toReplace.push([
-                  o.declarations.map((x) => x.id.name),
-                  o,
-                  value,
-                ]);
-              }
+              toReplace.push([names, o, value]);
             }
           };
         }
@@ -140,9 +138,9 @@ export default class MovedDeclarations extends Transform {
         varNames.delete(name);
       });
 
-      toReplace.forEach((x) => {
-        if (!x[0].find((x) => illegal.has(x))) {
-          this.replace(x[1], x[2]);
+      toReplace.forEach(([names, node1, node2]) => {
+        if (!names.find((name) => illegal.has(name))) {
+          this.replace(node1, node2);
         }
       });
 
@@ -151,8 +149,8 @@ export default class MovedDeclarations extends Transform {
         this.log("Moved", varNames);
 
         var variableDeclaration = VariableDeclaration(
-          Array.from(varNames).map((x) => {
-            return VariableDeclarator(x);
+          Array.from(varNames).map((name) => {
+            return VariableDeclarator(name);
           })
         );
 
