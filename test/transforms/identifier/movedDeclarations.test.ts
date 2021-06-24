637213ca275a9808bce9682a40f61456eb649820
MichaelXF <minimike3682@gmail.com>
2021-06-23 23:39:25 -0400

    Rewritten tests for `Moved Declarations`

--- test/transforms/identifier/movedDeclarations.test.ts
+++ test/transforms/identifier/movedDeclarations.test.ts
@@ -1,134 +1,159 @@
 import JsConfuser from "../../../src/index";
 
-it("should move variable declarations to the top of the lexical block", async () => {
-  var output = await JsConfuser.obfuscate(
-    `
-    console.log("...")
-    var a = 1, b = 2;
-    console.log(a)
-    `,
-    {
-      target: "node",
-      movedDeclarations: true,
-    }
-  );
+test("Variant #1: Move variable 'y' to top", async () => {
+  var code = `
+    var x = 10;
+    var y = 15;
+    TEST_VARIABLE = x + y;
+  `;
 
-  expect(output).toContain("var a,b;");
-  expect(output).toContain("a=1,b=2");
-});
+  var output = await JsConfuser.obfuscate(code, {
+    target: "node",
+    movedDeclarations: true,
+  });
 
-it("should still execute properly", async () => {
-  var output = await JsConfuser.obfuscate(
-    `
+  expect(output).toContain("var y;");
+  expect(output).toContain("y=15");
 
-    function add(n1, n2){
-      return n1 + n2;
-    }
-
-    var a = 6, b = 4;
-
-    input( add(a, b) );
-    `,
-    {
-      target: "node",
-      movedDeclarations: true,
-    }
-  );
-
-  expect(output).toContain("var a,b;");
-
-  var value = "never_called";
-  function input(valueIn) {
-    value = valueIn;
-  }
-
+  var TEST_VARIABLE;
   eval(output);
 
-  expect(value).toStrictEqual(10);
+  expect(TEST_VARIABLE).toStrictEqual(25);
 });
 
-it("should not move declarations in for statements", async () => {
-  var output = await JsConfuser.obfuscate(
-    `
-    for ( var i = 0; i < 10; i++ ) {
-      
-    }
-    `,
-    {
-      target: "node",
-      movedDeclarations: true,
-    }
-  );
+test("Variant #2: Move variable 'y' and 'z' to top", async () => {
+  var code = `
+    var x = 10;
+    var y = 15;
+    var z = 5;
+    TEST_VARIABLE = x + y + z;
+  `;
 
-  expect(output).toContain("for(var i=0;");
+  var output = await JsConfuser.obfuscate(code, {
+    target: "node",
+    movedDeclarations: true,
+  });
+
+  expect(output).toContain("var y,z");
+  expect(output).toContain("y=15");
+  expect(output).toContain("z=5");
+
+  var TEST_VARIABLE;
+  eval(output);
+
+  expect(TEST_VARIABLE).toStrictEqual(30);
 });
 
-it("should not move redefined names", async () => {
-  var output = await JsConfuser.obfuscate(
-    `
-    var a;
-    var output;
+test("Variant #2: Don't move 'y' (destructuring)", async () => {
+  var code = `
+    var x = 10;
+    var [y] = [15];
+    TEST_VARIABLE = x + y;
+  `;
+
+  var output = await JsConfuser.obfuscate(code, {
+    target: "node",
+    movedDeclarations: true,
+  });
+
+  expect(output).toContain("var [y]=[15];");
+
+  var TEST_VARIABLE;
+  eval(output);
+
+  expect(TEST_VARIABLE).toStrictEqual(25);
+});
+
+test("Variant #3: Don't move 'y' (nested lexical scope)", async () => {
+  var code = `
+    var x = 10;
+    var y = 15;
+
     (function(){
-      var a;
-      var output;
+      y = 10;
     })();
-    `,
-    {
-      target: "node",
-      movedDeclarations: true,
-    }
-  );
 
-  expect(output).toContain("var a;var output;");
+    TEST_VARIABLE = x + y;
+  `;
+
+  var output = await JsConfuser.obfuscate(code, {
+    target: "node",
+    movedDeclarations: true,
+  });
+
+  expect(output).toContain("var y=15");
+
+  var TEST_VARIABLE;
+  eval(output);
+
+  expect(TEST_VARIABLE).toStrictEqual(20);
 });
 
-it("should not move declarations in switch cases", async () => {
-  var output = await JsConfuser.obfuscate(
-    `
-    var b = 0;
-    switch(b){
-      case 0:
-        var a = 0;
-        break;
-      case 1:
-        a = 1;
-        break;
-    }
-    `,
-    {
-      target: "node",
-      movedDeclarations: true,
-    }
-  );
+test("Variant #4: Move 'y' (for statement initializer)", async () => {
+  var code = `
+    var x = 10;
+    for ( var y = 0; y < 15; y++ ) {
 
-  expect(output).toContain("var a=0;");
+    } // y ends as 15
+    TEST_VARIABLE = x + y;
+  `;
+
+  var output = await JsConfuser.obfuscate(code, {
+    target: "node",
+    movedDeclarations: true,
+  });
+
+  expect(output).not.toContain("var y=0;");
+
+  var TEST_VARIABLE;
+  eval(output);
+
+  expect(TEST_VARIABLE).toStrictEqual(25);
 });
 
-it("should not move declarations in nested switch cases", async () => {
-  var output = await JsConfuser.obfuscate(
-    `
-    var i = 1;
-    while(i > 0){
-      var b = 0;
-      switch(b){
-        case 0:
-          var a = 0;
-          break;
-        case 1:
-          a = 1;
-          break;
-      }
+test("Variant #5: Move 'y' (for-in left-hand initializer)", async () => {
+  var code = `
+    var x = 10;
+    for ( var y in [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] ) {
 
-      i--;
-    }
+    } // y ends as "15"
+    TEST_VARIABLE = x + parseInt(y);
+  `;
 
-    
-    `,
-    {
-      target: "node",
-      movedDeclarations: true,
-    }
-  );
+  var output = await JsConfuser.obfuscate(code, {
+    target: "node",
+    movedDeclarations: true,
+  });
 
-  expect(output).toContain("var a=0;");
+  expect(output).not.toContain("var y in");
+  expect(output).toContain("y in");
+
+  var TEST_VARIABLE;
+  eval(output);
+
+  expect(TEST_VARIABLE).toStrictEqual(25);
+});
+
+test("Variant #6: Don't move const or let variables", async () => {
+  var code = `
+    var fillerExpr;
+
+    let x = 10;
+    const y = 15;
+
+    TEST_VARIABLE = x + y;
+  `;
+
+  var output = await JsConfuser.obfuscate(code, {
+    target: "node",
+    movedDeclarations: true,
+  });
+
+  expect(output).toContain("let x=10");
+  expect(output).toContain("const y=15");
+
+  var TEST_VARIABLE;
+  eval(output);
+
+  expect(TEST_VARIABLE).toStrictEqual(25);
 });
