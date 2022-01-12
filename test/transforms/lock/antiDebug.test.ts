43ad663f647b69e4fb74c263d15236047d37ea0d
MichaelXF <minimike3682@gmail.com>
2022-01-11 21:39:50 -0500

    Fix #28

--- test/transforms/lock/antiDebug.test.ts
+++ test/transforms/lock/antiDebug.test.ts
@@ -21,3 +21,46 @@ it("add a background interval", async () => {
 
   expect(output).toContain("setInterval");
 });
+
+it("should place syntax-correct code", async () => {
+  for (var i = 0; i < 50; i++) {
+    var output = await JsConfuser.obfuscate(
+      `
+    /**
+    * GitHub: https://github.com/MichaelXF/js-confuser
+    * NPM: https://www.npmjs.com/package/js-confuser
+    *
+    * Welcome to Js Confuser
+    * 
+    * You can obfuscate the code with the top right button 'Obfuscate'.
+    * 
+    * You can customize the obfuscator with the button 'Options'.
+    * (Set the target to 'node' for NodeJS apps)
+    *
+    * Happy Hacking!
+    */
+    
+    function greet(name){
+        var output = "Hello " + name + "!";
+    }
+    
+    greet("Internet User");
+    
+    `,
+      {
+        compact: true,
+        controlFlowFlattening: 0.25,
+        identifierGenerator: "randomized",
+        lock: { antiDebug: true },
+        minify: true,
+        target: "node",
+      }
+    );
+
+    try {
+      eval(output);
+    } catch (e) {
+      expect(e).toStrictEqual(undefined);
+    }
+  }
+});
