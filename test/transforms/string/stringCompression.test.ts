866c0e189a80b1f1ffe1adf127b09b0181651060
MichaelXF <minimike3682@gmail.com>
2023-05-28 21:15:02 -0400

    fix: stringCompression custom callback + optimizations

--- test/transforms/string/stringCompression.test.ts
+++ test/transforms/string/stringCompression.test.ts
@@ -79,3 +79,42 @@ it("should not encode constructor key", async () => {
 
   expect(TEST_VAR).toStrictEqual(100);
 });
+
+it("should be configurable by custom function option", async () => {
+  var code = `
+  TEST_OUTPUT_1 = "My String 1";
+  TEST_OUTPUT_2 = "My String 2";
+  TEST_OUTPUT_3 = "My String 3";
+  `;
+
+  var stringsFound = [];
+
+  var output = await JsConfuser(code, {
+    target: "node",
+    stringCompression: (strValue) => {
+      stringsFound.push(strValue);
+
+      // Change all strings but "My String 2"
+      return strValue !== "My String 2";
+    },
+  });
+
+  // Ensure all strings were found
+  expect(stringsFound).toContain("My String 1");
+  expect(stringsFound).toContain("My String 2");
+  expect(stringsFound).toContain("My String 3");
+
+  // Ensure the strings got changed (except for "My String 2")
+  expect(output).not.toContain("TEST_OUTPUT_1='My String 1'");
+  expect(output).toContain("TEST_OUTPUT_2='My String 2'");
+  expect(output).not.toContain("TEST_OUTPUT_3='My String 3'");
+
+  // Make sure the code still works!
+  var TEST_OUTPUT_1, TEST_OUTPUT_2, TEST_OUTPUT_3;
+
+  eval(output);
+
+  expect(TEST_OUTPUT_1).toStrictEqual("My String 1");
+  expect(TEST_OUTPUT_2).toStrictEqual("My String 2");
+  expect(TEST_OUTPUT_3).toStrictEqual("My String 3");
+});
